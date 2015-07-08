9ea5c3d9a96515da7ff6072b3d49a62e19592ddb
Ahura Mazda <theahuramazda@gmail.com>
2015-07-08 20:09:55 +0100

    removed: unecessary jquery and bootstrap

---
+++ Galleria.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Galleria.Web
+{
+	public class BundleConfig
+	{
+		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			/* JavaScript bundles */
+			bundles.Add(new ScriptBundle("~/scripts/angular").Include("~/Scripts/angular*"));
+
+
+			/* CSS/Style bundles */
+			bundles.Add(new StyleBundle("~/styles/").Include("~/styles/*.css"));
+		}
+	}
+}
--- Galleria.Web/Global.asax.cs
+++ Galleria.Web/Global.asax.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Mvc;
 
 namespace Galleria.Web
 {
-    public class WebApiApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-        }
-    }
+	public class WebApiApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+
+			AreaRegistration.RegisterAllAreas();
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);			
+		}
+	}
 }
