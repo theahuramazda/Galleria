a2af5cbb456a0e4d5d44c75008dd976fcea78370
Ahura Mazda <theahuramazda@gmail.com>
2015-07-08 22:22:45 +0100

    added: galleria menu directive

---
+++ Galleria.Web/angular-app/directives/galleria.directive.menu.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="../../scripts/angular.js" />
+(function ()
+{
+	angular.module("GalleriaApp")
+		.controller("GalleriaMenuController", GalleriaMenuController)
+		.directive("galleriaMenu", GalleriaDirective);
+
+	function GalleriaDirective()
+	{
+		return {
+			restrict: "E",
+			templateUrl: "/angular-app/directives/galleria.directive.menu.html",
+			controller: "GalleriaMenuController",
+			controllerAs: "GalleriaMenuViewModel"
+		};
+	}
+
+	GalleriaMenuController.$inject = ["$scope"];
+	function GalleriaMenuController()
+	{
+
+	}
+})()
