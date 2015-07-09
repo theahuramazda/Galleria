fac633132fca0da2f3251f91240c753bac727bd8
Ahura Mazda <theahuramazda@gmail.com>
2015-07-09 18:07:34 +0100

    added: menu controller for menu directive

---
+++ Galleria.Web/angular-app/controllers/galleria.controller.menu.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="../../scripts/angular.js" />
+(function(){
+	angular.module("GalleriaApp")
+		.controller("GalleriaMenuController", GalleriaMenuController);
+
+	GalleriaMenuController.$inject = ["$scope", "menuService"];
+	function GalleriaMenuController($scope, menuService) {
+		var galleriaMenu = this;
+
+		//TODO: replace with service
+		var tempVariable = ["Menu 1", "Menu 2", "Menu 3"]
+
+		galleriaMenu.Menus = menuService.getMenuItems();
+	}
+})();
