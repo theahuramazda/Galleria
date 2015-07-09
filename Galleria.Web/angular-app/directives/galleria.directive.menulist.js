06cea9eba0a5a5157776732379cb21a3e4ccd440
Ahura Mazda <theahuramazda@gmail.com>
2015-07-09 19:46:28 +0100

    updated: renamed GalleriaMenu to GalleriaMenuList

---
+++ Galleria.Web/angular-app/directives/galleria.directive.menulist.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../../scripts/angular.js" />
+(function () {
+	angular.module("GalleriaApp")
+		.directive("galleriaMenuList", GalleriaMenuListDirective);
+
+	function GalleriaMenuListDirective()
+	{
+		return {
+			restrict: "E",
+			templateUrl: "/angular-app/directives/galleria.directive.menulist.html"
+		};
+	}
+})()
